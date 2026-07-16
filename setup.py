8c5b5ccadf905047e9c51cb56c7dc4c012756e3d
Prathamesh Zagade <prathamzagade1811@gmail.com>
2026-07-16 14:25:51 +0530

    new files

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 from setuptools import find_packages , setup
+from typing import List
 
+hyphen_e_dot = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        [req.replace("\n","") for req in requirements]
+
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot)
+    return requirements
 
 
 setup(
@@ -8,7 +23,5 @@ version='0.0.1',
 author='Pratham',
 author_email='prathamzagade1811@gmail.com',
 packages=find_packages(),
-install_requires=['pandas','numpy','seaborn',]
-
-
+install_requires=get_requirements('requirements.txt')
 )
