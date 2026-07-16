54c513d9d3a2b14860dd29a68636319fa48b1d23
Prathamesh Zagade <prathamzagade1811@gmail.com>
2026-07-16 14:35:01 +0530

    setup.py and requirements.txt

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # ML-Project
 
 ## (base) pratham@MacBook-Air ML Project % conda activate ./myenv
+
+## Agenda
+
+1. Set up the GitHub Repository
+   - a) New environment
+   - b) `setup.py`
+   - c) `requirements.txt`
+
+2. Src folder and build the package
--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from typing import List
 
 hyphen_e_dot = '-e .'
 
-def get_requirements(file_path:str)->List[str]:
+def get_requirements(file_path: str) -> List[str]:
     '''
     This function will return the list of requirements
     '''
-    requirements=[]
+    requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        [req.replace("\n","") for req in requirements]
+        requirements = [req.replace("\n", "") for req in requirements]
 
         if hyphen_e_dot in requirements:
             requirements.remove(hyphen_e_dot)
